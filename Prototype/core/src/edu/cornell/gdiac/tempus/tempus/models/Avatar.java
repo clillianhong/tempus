380ab2c4253a75a4a3922c5edd9616c3ec3c28ce
Maxwell Tomsick <mit23@cornell.edu>
2020-04-23 23:50:23 -0400

    added textures for platforms in both worlds

--- Prototype/core/src/edu/cornell/gdiac/tempus/tempus/models/Avatar.java
+++ Prototype/core/src/edu/cornell/gdiac/tempus/tempus/models/Avatar.java
@@ -843,17 +843,6 @@ public class Avatar extends CapsuleObstacle {
     public void update(float dt) {
         //System.out.println(lives);
         // Apply cooldowns
-        if (enemyContact  && enemyTicks == 0){
-            enemyTicks = 10;
-        }
-        if (enemyTicks > 0){
-            enemyTicks --;
-        }
-        if (enemyTicks == 1 && enemyContact){
-            enemyTicks = 0;
-            removeLife();
-            enemyContact = false;
-        }
         if (hitByProjctile && isHolding){
                 removeLife();
                 hitByProjctile = false;
