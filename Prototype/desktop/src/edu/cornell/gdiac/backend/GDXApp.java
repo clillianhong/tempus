3885da8b42453d6bc33c3e3d465321a44eae23ac
clillianhong <c.lillianhong@gmail.com>
2020-05-02 21:27:04 -0400

    walker's fix

--- Prototype/desktop/src/edu/cornell/gdiac/backend/GDXApp.java
+++ Prototype/desktop/src/edu/cornell/gdiac/backend/GDXApp.java
@@ -1,27 +1,33 @@
 /*
  * GDXApp.java
  *
- * This is a (necessary) replacement for LwjglApplication. It allows us to disable audio and then
- * re-enable it later. It also allows us to force GL30.
+ * This is a replacement for LwjglApplication. It allows us to force GL30.  Originally we used this class
+ * to force our new audio engine (and we hope to do that again someday), but access issues forced us to
+ * place a proxy class in com.badlogic.gdx.backends.lwjgl.
  *
  * @author Walker M. White
  * @data April 10, 2020
  */
 package edu.cornell.gdiac.backend;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Audio;
 import com.badlogic.gdx.backends.lwjgl.*;
+import com.badlogic.gdx.backends.lwjgl.audio.OpenALAudio;
 
 /**
  * This class represents a desktop game application using Lwjgl.
  *
  * This class is preferable to {@link LwjglApplication} because it is guaranteed to use GL30, and it has
- * a slightly more useful audio engine.
+ * a slightly more useful audio engine.  It extends {@link LwjglProxy} because we needed to access some
+ * package internals.
  */
-public class GDXApp extends LwjglApplication {
+public class GDXApp extends LwjglProxy {
     /** Settings, remembered for second initialization phase */
-    GDXAppSettings config;
+    protected GDXAppSettings config;
+
+    /** The replacement audio engine */
+    protected GDXAudio engine;
 
     /**
      * Creates a game application with the given listener and settings.
@@ -38,25 +44,89 @@ public class GDXApp extends LwjglApplication {
     }
 
     /**
-     * Invokes the main application loop.
-     *
-     * This is a replacement to {@link LwjglApplication#mainLoop()} that prepends it with an second
-     * initialization phase.  This is the phase where we generate our audio engine.
+     * Return the settings for this application
+     */
+    public GDXAppSettings getSettings() {
+        return config;
+    }
+
+    /**
+     * Encapsulate audio initialization so it can be overridden.
      */
     @Override
-    protected void mainLoop() {
-        try {
-            audio = new GDXAudio(config.audioDeviceSimultaneousSources,
-                                 config.audioDeviceBufferCount,
-                                 config.audioDeviceBufferSize);
-            Gdx.audio = audio;
-            LwjglApplicationConfiguration.disableAudio = false;
-        } catch (Throwable t) {
-            log("GDXApp", "Couldn't initialize audio, disabling audio", t);
-            LwjglApplicationConfiguration.disableAudio = true;
-            config.useAudio = false;
+    protected void initAudio(LwjglApplicationConfiguration config) {
+        if (!LwjglApplicationConfiguration.disableAudio) {
+            try {
+                // Piece of sh*t OpenAL only guarantees 16 sources, so we need to be guilty
+                audio = new OpenALAudio(config.audioDeviceSimultaneousSources,
+                        config.audioDeviceBufferCount,
+                        config.audioDeviceBufferSize);
+            } catch (Throwable t) {
+                log("GDXApp", "Couldn't initialize audio; disabling audio", t);
+                LwjglApplicationConfiguration.disableAudio = true;
+            }
         }
-        super.mainLoop();
+
+        boolean fallback = false;
+        if (!LwjglApplicationConfiguration.disableAudio) {
+            try {
+                // No that we have initialized OpenAL, steel its resources
+                engine = new GDXAudio(config.audioDeviceSimultaneousSources,
+                        config.audioDeviceBufferCount,
+                        config.audioDeviceBufferSize);
+            } catch (Throwable t) {
+                log("GDXApp", "Couldn't initialize secondary audio; falling back", t);
+                engine = null;
+                fallback = true;
+            }
+        }
+
+        if (fallback) {
+            try {
+                // Dispose of the audio engine and reinitialize
+                if (audio != null) { audio.dispose(); }
+                audio = new OpenALAudio(config.audioDeviceSimultaneousSources,
+                        config.audioDeviceBufferCount,
+                        config.audioDeviceBufferSize);
+            } catch (Throwable t) {
+                log("LwjglApplication", "Couldn't reinitialize audio; disabling audio", t);
+                LwjglApplicationConfiguration.disableAudio = true;
+            }
+        }
+    }
+
+    /**
+     * Disposes the audio on cleanup.
+     */
+    @Override
+    protected void disposeAudio() {
+        if (engine != null) { engine.dispose(); }
+        if (audio != null) { audio.dispose(); }
+    }
+
+    /**
+     * Return the active audio engine.
+     */
+    @Override
+    protected Audio chooseAudio() {
+        if (engine != null) {
+            return engine;
+        } else if (audio != null) {
+            return audio;
+        }
+        return null;
+    }
+
+    /**
+     * Updates the audio loop for any PCM buffering
+     */
+    @Override
+    protected void updateAudio() {
+        if (engine != null) {
+            engine.update();
+        } else if (audio != null) {
+            audio.update();
+        }
     }
 
 }
